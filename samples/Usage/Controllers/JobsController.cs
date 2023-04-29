4c618a958ac1e99121900b7d9ae7ff6f66f22dff
Aydin Mir Mohammadi <am@bluehands.de>
2023-04-29 21:18:38 +0200

    Update reference

--- samples/Usage/Controllers/JobsController.cs
+++ samples/Usage/Controllers/JobsController.cs
@@ -23,14 +23,21 @@ namespace Usage.Controllers
         public async Task<IActionResult> Get()
         {
 
-            m_RecurringJobs.AddOrUpdateCarbonAware("minutely", () => Console.WriteLine("Hello, world!"), Cron.Minutely, TimeSpan.FromHours(2),TimeSpan.FromMinutes(20));
+            m_RecurringJobs.AddOrUpdateCarbonAware("daily", () => Console.WriteLine("Hello, world!"), Cron.Daily, TimeSpan.FromHours(2),TimeSpan.FromMinutes(20));
             
+            CarbonAwareRecurringJob.AddOrUpdate(
+                "daily", 
+                () => Console.WriteLine("Hello, world!"), 
+                Cron.Daily, TimeSpan.FromHours(2), 
+                TimeSpan.FromMinutes(20));
+
+
             await m_BackgroundJobs.EnqueueWithCarbonAwarenessAsync(
                 () => Console.WriteLine("Hello world from Hangfire!. Enqueue carbon aware jobs"),
                 DateTimeOffset.Now + TimeSpan.FromHours(2),
                 TimeSpan.FromMinutes(5));
             
-            await CarbonAwareBackgroundJob.Enqueue(
+            await CarbonAwareBackgroundJob.EnqueueAsync(
                 () => Console.WriteLine("Hello world from Hangfire!. Enqueue carbon aware jobs"),
                 DateTimeOffset.Now + TimeSpan.FromHours(2),
                 TimeSpan.FromMinutes(5));
@@ -41,7 +48,7 @@ namespace Usage.Controllers
                 TimeSpan.FromMinutes(20),
                 TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(5));
 
-            await CarbonAwareBackgroundJob.Schedule(
+            await CarbonAwareBackgroundJob.ScheduleAsync(
                 () => Console.WriteLine("Hello world from Hangfire!. Schedule carbon aware jobs"),
                 DateTimeOffset.Now + TimeSpan.FromHours(2),
                 TimeSpan.FromMinutes(20),
